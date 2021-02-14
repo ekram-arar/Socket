c54dce0a294fadff2b28593c70933ad85f4f7e5a
Your Name <you@example.com>
2021-02-14 16:28:09 +0200

    first commit

---
+++ Back/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+ 
+const server = app.listen(8000, () =>
+  console.log('The server is all fired up on port 8000')
+);
+ 
+// To initialize the socket, we need to
+// invoke the socket.io library
+// and pass it our Express server
+const io = require('socket.io')(server, { cors: true });
+
+
+io.on("connection", socket => {
+    console.log(socket.id);
+    console.log("Nice to meet you. (shake hand)");
+    socket.emit("Welcome", {msg: "hakuna matata"});
+   //socket.on("Hello", ()=> console.log({msg: "hi"}));
+});
+
